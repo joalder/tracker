dfa026482b510acff6b80e44a2a4124940c8c5f7
joalder <jonas.alder@gmail.com>
2015-10-09 21:44:57 +0200

    added more layout (button, fields) to the new task window

--- src/ch/vilalde/tracker/NewTaskWindow.java
+++ src/ch/vilalde/tracker/NewTaskWindow.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.Vector;
 
 /**
  * Form for entering new tasks and projects
@@ -28,12 +29,90 @@ public class NewTaskWindow extends JFrame {
     }
 
     private void prepareUi() {
-        // A proper SpringLayout would probably be appropriate here
-        mainPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 6, 10));
+        mainPanel = new JPanel(new GridBagLayout());
 
-        JLabel label = new JLabel("Even here will be some content");
+        GridBagConstraints constraints = new GridBagConstraints();
 
-        mainPanel.add(label);
+        JTextField fieldTitle = new JTextField(30);
+        constraints.gridx = 1;
+        constraints.gridy = 0;
+        constraints.gridwidth = 2;
+        constraints.ipady = 8;
+        constraints.ipadx = 5;
+        constraints.insets = new Insets(5, 5, 5, 5);
+        mainPanel.add(fieldTitle, constraints);
+
+        JLabel labelTitle = new JLabel("Title:");
+        labelTitle.setLabelFor(fieldTitle);
+        constraints.anchor = GridBagConstraints.EAST;
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.gridwidth = 1;
+        mainPanel.add(labelTitle, constraints);
+
+        // @Todo: Actually fill in the current projects
+        JComboBox<String> fieldProject = new JComboBox<>();
+        constraints.gridx = 1;
+        constraints.gridy = 1;
+        mainPanel.add(fieldProject, constraints);
+
+        JLabel labelProject = new JLabel("Project:");
+        labelProject.setLabelFor(fieldProject);
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        mainPanel.add(labelProject, constraints);
+
+        JButton buttonProject = new JButton("Add new project");
+        constraints.gridx = 2;
+        constraints.gridy = 1;
+        mainPanel.add(buttonProject, constraints);
+
+        // tbd: Effort Field
+
+        JLabel labelEstEffort = new JLabel("Estimated Effort:");
+        //labelEstEffort.setLabelFor(fieldProject);
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        mainPanel.add(labelEstEffort, constraints);
+
+        Vector<String> prioChoice = new Vector<>();
+        prioChoice.add("High");
+        prioChoice.add("Medium");
+        prioChoice.add("Low");
+        JComboBox<String> fieldPriority = new JComboBox<>(prioChoice);
+        constraints.gridx = 1;
+        constraints.gridy = 3;
+        mainPanel.add(fieldPriority, constraints);
+
+        JLabel labelPriority = new JLabel("Priority:");
+        labelPriority.setLabelFor(fieldPriority);
+        constraints.gridx = 0;
+        constraints.gridy = 3;
+        mainPanel.add(labelPriority, constraints);
+
+        JTextArea textArea = new JTextArea(6, 30);
+        constraints.gridx = 1;
+        constraints.gridy = 4;
+        constraints.gridwidth = 2;
+        mainPanel.add(textArea, constraints);
+
+        JLabel labelDetailDescription = new JLabel("Detailed Description:");
+        labelPriority.setLabelFor(textArea);
+        constraints.gridx = 0;
+        constraints.gridy = 4;
+        constraints.gridwidth = 1;
+        mainPanel.add(labelDetailDescription, constraints);
+
+        JButton buttonSave = new JButton("Save");
+        constraints.gridx = 1;
+        constraints.gridy = 5;
+        mainPanel.add(buttonSave, constraints);
+
+        JButton buttonSaveAndAdd = new JButton("Save and add another");
+        constraints.gridx = 2;
+        constraints.gridy = 5;
+        mainPanel.add(buttonSaveAndAdd, constraints);
+
         this.add(mainPanel);
     }
 
